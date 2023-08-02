c6b9071fbd1446bf173f5ee384abe4e39f62e1cb
Karina Zubko <karinazubko2017@gmail.com>
2023-08-02 12:57:21 +0300

    Overriding onCreateView(…) (CrimeDetailFragment.kt)

--- app/src/main/java/com/example/criminalintent/CrimeDetailFragment.kt
+++ app/src/main/java/com/example/criminalintent/CrimeDetailFragment.kt
@@ -1,11 +1,16 @@
 package com.example.criminalintent
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.criminalintent.databinding.FragmentCrimeDetailBinding
 import java.util.Date
 import java.util.UUID
 
 class CrimeDetailFragment : Fragment() {
+    private lateinit var binding: FragmentCrimeDetailBinding
     private lateinit var crime: Crime
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +22,13 @@ class CrimeDetailFragment : Fragment() {
             date = Date(),
             isSolved = false
         )
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentCrimeDetailBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 }
