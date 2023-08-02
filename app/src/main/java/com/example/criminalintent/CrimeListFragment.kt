f2dbd8cda084b97febc88317f6efa9542440065b
Karina Zubko <karinazubko2017@gmail.com>
2023-08-02 14:15:02 +0300

    Updating the list item layout file (layout/list_item_crime.xml)

--- app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.criminalintent.databinding.FragmentCrimeListBinding
 
 private const val TAG = "CrimeListFragment"
@@ -28,6 +29,7 @@ class CrimeListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentCrimeListBinding.inflate(inflater, container, false)
+        binding.crimeRecyclerView.layoutManager = LinearLayoutManager(context)
         return binding.root
     }
     override fun onDestroyView() {
