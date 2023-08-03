f7c4bc5a8cf49dc72a57cccce59c3fec0b4aa54f
Karina Zubko <karinazubko2017@gmail.com>
2023-08-03 09:49:10 +0300

    Launching your first coroutine (CrimeListViewModel.kt)

--- app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
@@ -1,21 +1,25 @@
 package com.example.criminalintent
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 import java.util.Date
 import java.util.UUID
 
 class CrimeListViewModel : ViewModel() {
     val crimes = mutableListOf<Crime>()
     init {
-        for (i in 0 until 100) {
-            val crime = Crime(
-                id = UUID.randomUUID(),
-                title ="Crime #$i",
-                date = Date(),
-                isSolved = i % 2 == 0,
-                requiresPolice = i % 5 == 0
-            )
-            crimes += crime
+        viewModelScope.launch {
+            for (i in 0 until 100) {
+                val crime = Crime(
+                    id = UUID.randomUUID(),
+                    title = "Crime #$i",
+                    date = Date(),
+                    isSolved = i % 2 == 0,
+                    requiresPolice = i % 5 == 0
+                )
+                crimes += crime
+            }
         }
     }
 }
