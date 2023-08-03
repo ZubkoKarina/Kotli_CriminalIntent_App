1452766e04205854befd707d5cb70a70641a8334
Karina Zubko <karinazubko2017@gmail.com>
2023-08-03 10:07:50 +0300

    Defining your own suspending function (CrimeListViewModel.kt)

--- app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
@@ -2,6 +2,7 @@ package com.example.criminalintent
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import java.util.Date
 import java.util.UUID
@@ -10,16 +11,22 @@ class CrimeListViewModel : ViewModel() {
     val crimes = mutableListOf<Crime>()
     init {
         viewModelScope.launch {
-            for (i in 0 until 100) {
-                val crime = Crime(
-                    id = UUID.randomUUID(),
-                    title = "Crime #$i",
-                    date = Date(),
-                    isSolved = i % 2 == 0,
-                    requiresPolice = i % 5 == 0
-                )
-                crimes += crime
-            }
+            crimes += loadCrimes()
         }
+    }
+    suspend fun loadCrimes(): List<Crime> {
+        val result = mutableListOf<Crime>()
+        delay(5000)
+        for (i in 0 until 100) {
+            val crime = Crime(
+                id = UUID.randomUUID(),
+                title = "Crime #$i",
+                date = Date(),
+                isSolved = i % 2 == 0,
+                requiresPolice = i % 5 == 0
+            )
+            result += crime
+        }
+        return result
     }
 }
