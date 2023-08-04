398ffb37c2c1a33b142ef28a5f292fb75b8f8578
Karina Zubko <karinazubko2017@gmail.com>
2023-08-04 15:45:51 +0300

    Adding a listener to the EditText view (CrimeDetailFragment.kt)

--- app/src/main/java/com/example/criminalintent_new/CrimeDetailFragment.kt
+++ app/src/main/java/com/example/criminalintent_new/CrimeDetailFragment.kt
@@ -1,21 +1,43 @@
 package com.example.criminalintent_new
-
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import com.example.criminalintent_new.databinding.FragmentCrimeDetailBinding
 import java.util.Date
 import java.util.UUID
 
 class CrimeDetailFragment : Fragment() {
+    private lateinit  var binding: FragmentCrimeDetailBinding
     private lateinit var crime : Crime
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         crime = Crime(
             id = UUID.randomUUID(),
             title = "",
             date = Date(),
             isSolved = false
         )
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding =
+            FragmentCrimeDetailBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            crimeTitle.doOnTextChanged{text, _, _, _ ->
+                crime = crime.copy(title = text.toString())
+            }
+        }
     }
 }
