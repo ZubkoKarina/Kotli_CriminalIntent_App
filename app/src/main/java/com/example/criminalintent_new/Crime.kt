c40cc288abddaabefff77649a89507a7f07f0cd6
Karina Zubko <karinazubko2017@gmail.com>
2023-08-04 14:51:57 +0300

    Adding the Crime data class (Crime.kt)

---
+++ app/src/main/java/com/example/criminalintent_new/Crime.kt
@@ -0,0 +1,11 @@
+package com.example.criminalintent_new
+
+import java.util.Date
+import java.util.UUID
+
+data class Crime(
+    val id: UUID,
+    val title: String,
+    val date: Date,
+    val isSolved: Boolean
+)
